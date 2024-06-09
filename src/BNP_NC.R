1673f9775e6037bfc42475ed4656ca7b25ce13ef
Jie Hu <kate@Jies-MacBook-Pro.local>
2024-06-09 19:53:19 -0400

    reformat

--- src/BNP_NC.R
+++ src/BNP_NC.R
@@ -8,17 +8,16 @@
 # --- Gibbs sampler ---
 
 DDP_ADJ <- function(s_seed = 1,
-                    X_tilde, 
+                    X_tilde,
                     X_W_reg,
                     X_split_method,
-                    probs = NULL, 
-                    pts = NULL, 
+                    probs = NULL,
+                    pts = NULL,
                     R = R,
                     R_burnin,
                     n_group,
-                    n=n,
-                    data_analysis){
-  
+                    n = n,
+                    data_analysis) {
   # seed for reproducibility
   set.seed(s_seed)
   
@@ -29,7 +28,7 @@ DDP_ADJ <- function(s_seed = 1,
   dim_W = dim(X_W_reg)[2]
   if (X_split_method == "split_quantile") {
     X_weights = split_quantile(data_analysis$X, probs = probs)
-  } else if (X_split_method == "split_at_fixed_pt"){
+  } else if (X_split_method == "split_at_fixed_pt") {
     X_weights = split_at_fixed_pt(data_analysis$X, pts = pts)
   } else{
     X_weights = split_4quantile(data_analysis$X)
@@ -51,7 +50,7 @@ DDP_ADJ <- function(s_seed = 1,
   gamma_y_prior = c(1, 0.5)
   
   # parameters for gamma_w \sim InvGamma
-  # gamma_w = variance of the distribution of W|X,Z 
+  # gamma_w = variance of the distribution of W|X,Z
   gamma_w_prior = c(2, 2)
   # eta's iperparameters
   # eta = parameters in the probit regression in the weights
@@ -108,10 +107,10 @@ DDP_ADJ <- function(s_seed = 1,
   post_delta = matrix(NA, nrow = dim_W, ncol = R)
   post_beta_x = matrix(NA, nrow = n_group, ncol = R)
   post_intercept = matrix(NA, nrow = n_group, ncol = R)
-  post_eta = matrix(NA, nrow = (n_group - 1) * (dim(X_weights)[2]), ncol=R)
+  post_eta = matrix(NA, nrow = (n_group - 1) * (dim(X_weights)[2]), ncol =
+                      R)
   
   for (r in 1:R) {
-    
     #######################################################
     # -----  estimation parameter for W|X,Z model  ------
     #######################################################
@@ -165,13 +164,13 @@ DDP_ADJ <- function(s_seed = 1,
     check_q = sapply(1:(n_group - 1), function(l)
       length(Q[K >= l, l]))
     v_eta = lapply(which(check_q > 3), function(l)
-      1 / eta_prior[2] + t(X_weights[K >= l, ]) %*% X_weights[K >= l, ])
+      1 / eta_prior[2] + t(X_weights[K >= l,]) %*% X_weights[K >= l,])
     m_eta = sapply(which(check_q > 3), function(l)
       eta_prior[1] / eta_prior[2] +
-        t(X_weights[K >= l, ]) %*% Q[K >= l, l] / gamma_y[l])
+        t(X_weights[K >= l,]) %*% Q[K >= l, l] / gamma_y[l])
     # each component has to have at least 3 units to estimate eta
     if (any(check_q < 4)) {
-      for (l in which(check_q == 1 | check_q == 2|check_q == 3)) {
+      for (l in which(check_q == 1 | check_q == 2 | check_q == 3)) {
         v_eta[[l]] = diag(dim(X_weights)[2]) / eta_prior[2]
         m_eta = cbind(m_eta, rep(eta_prior[1] / eta_prior[2], dim(X_weights)[2]))
       }
@@ -181,14 +180,16 @@ DDP_ADJ <- function(s_seed = 1,
                                                   function(l)
                                                     rmvnorm(1, solve(v_eta[[l]]) %*% m_eta[, l]))
     eta[, which(table_Ki[-n_group] == 0)] = rnorm(length(which(table_Ki[-n_group] ==
-                                                                 0)) * dim(X_weights)[2], eta_prior[1], sqrt(eta_prior[2]))
+                                                                 0)) * dim(X_weights)[2],
+                                                  eta_prior[1],
+                                                  sqrt(eta_prior[2]))
     
     
     ##############################################
     # -------- 2: Component Allocation  ---------
     ##############################################
     
-    #  tau = recursive weigths in the mixture 
+    #  tau = recursive weigths in the mixture
     eta_X = pnorm(cbind(X_weights %*% (eta[, 1]), X_weights %*% (eta[, 2])))
     for (g_k in 3:(n_group - 1)) {
       eta_X = cbind(eta_X, pnorm(X_weights %*% (eta[, g_k])))
@@ -210,13 +211,13 @@ DDP_ADJ <- function(s_seed = 1,
     
     tau = tau_prov / apply(tau_prov, 1, sum)
     for (i in which(is.na(tau[, 1]))) {
-      tau[i, ] = 1 / n_group
+      tau[i,] = 1 / n_group
     }
     
     # K = allocation component variables
     # it says each unit in which component of the mixture is allocated
     K = sapply(1:n, function(i)
-      (1:n_group) %*% rmultinom(1, 1, tau[i, ]))
+      (1:n_group) %*% rmultinom(1, 1, tau[i,]))
     table_Ki = rep(0, n_group)
     table_Ki[sort(unique(K))] = table(K)
     
@@ -225,22 +226,22 @@ DDP_ADJ <- function(s_seed = 1,
     ##############################################
     
     # alpha + gamma_y :
-    # parameters of the mean + variance in each mixture components of the distribution of Y|X,Z 
+    # parameters of the mean + variance in each mixture components of the distribution of Y|X,Z
     for (g_k in 1:n_group) {
       if (length(which(K == g_k)) == 1) {
-        V = diag(dim_REG) / alpha_sigma + t(t(X_tilde[K == g_k, ])) %*% t(X_tilde[K ==
-                                                                                    g_k, ]) / gamma_y[g_k]
-        M = alpha_mu / alpha_sigma + t(t(X_tilde[K == g_k, ])) %*% t(data_analysis$Y[K ==
-                                                                                       g_k]) / gamma_y[g_k]
+        V = diag(dim_REG) / alpha_sigma + t(t(X_tilde[K == g_k,])) %*% t(X_tilde[K ==
+                                                                                   g_k,]) / gamma_y[g_k]
+        M = alpha_mu / alpha_sigma + t(t(X_tilde[K == g_k,])) %*% t(data_analysis$Y[K ==
+                                                                                      g_k]) / gamma_y[g_k]
       } else{
-        V = diag(dim_REG) / alpha_sigma + t(X_tilde[K == g_k, ]) %*% X_tilde[K ==
-                                                                               g_k, ] / gamma_y[g_k]
-        M = alpha_mu / alpha_sigma + t(X_tilde[K == g_k, ]) %*% data_analysis$Y[K ==
-                                                                                  g_k] / gamma_y[g_k]
+        V = diag(dim_REG) / alpha_sigma + t(X_tilde[K == g_k,]) %*% X_tilde[K ==
+                                                                              g_k,] / gamma_y[g_k]
+        M = alpha_mu / alpha_sigma + t(X_tilde[K == g_k,]) %*% data_analysis$Y[K ==
+                                                                                 g_k] / gamma_y[g_k]
       }
       alpha[, g_k] = t(rmvnorm(1, solve(V) %*% M, solve(V)))
       
-      G = sum((data_analysis$Y[K == g_k] - X_tilde[K == g_k, ] %*% (alpha[, g_k])) ^
+      G = sum((data_analysis$Y[K == g_k] - X_tilde[K == g_k,] %*% (alpha[, g_k])) ^
                 2)
       gamma_y[g_k] = rinvgamma(1, gamma_y_prior[1] + (sum(K == g_k)) / 2, gamma_y_prior[2] +
                                  G / 2)
@@ -252,7 +253,7 @@ DDP_ADJ <- function(s_seed = 1,
     ##############################################
     
     #beta_x + intercept
-    beta_x = alpha[2, ] - alpha[3, ] * delta[1, 2] / delta[1, 3]
+    beta_x = alpha[2,] - alpha[3,] * delta[1, 2] / delta[1, 3]
     if (dim_REG < 4) {
       intercept = sapply(1:n_group, function(g)
         mean(
@@ -278,7 +279,8 @@ DDP_ADJ <- function(s_seed = 1,
     post_intercept[, r] = intercept
     
     #
-    if (r%%500==0) print(paste0(r,"/",R," iterations"))
+    if (r %% 500 == 0)
+      print(paste0(r, "/", R, " iterations"))
   }
   
   return(
@@ -293,19 +295,18 @@ DDP_ADJ <- function(s_seed = 1,
 }
 
 
-curve_ADJ <- function(x, 
-                      post_chain, 
-                      X_tilde, 
+curve_ADJ <- function(x,
+                      post_chain,
+                      X_tilde,
                       x_split_method,
-                      probs = NULL, 
-                      pts = NULL, 
+                      probs = NULL,
+                      pts = NULL,
                       data_analysis,
                       n_group) {
-
   # preparing matrix for the probit regression in the weights
   if (x_split_method == "split_quantile_x") {
     X_weights = split_quantile_x(data_analysis$X, x, probs = probs)
-  } else if (x_split_method == "split_at_fixed_pt_x"){
+  } else if (x_split_method == "split_at_fixed_pt_x") {
     X_weights = split_at_fixed_pt_x(data_analysis$X, x, pts = pts)
   } else{
     X_weights = split_4quantile_x(data_analysis$X, x)
@@ -317,14 +318,14 @@ curve_ADJ <- function(x,
   
   # estimation weights for a grid of values of treatment X
   c_eta_X = pnorm(cbind(t(X_weights %*% (
-    post_chain$post_eta[1:dim_Xw, ]
+    post_chain$post_eta[1:dim_Xw,]
   )),
   t(X_weights %*% (
-    post_chain$post_eta[(dim_Xw + 1):(dim_Xw * 2), ]
+    post_chain$post_eta[(dim_Xw + 1):(dim_Xw * 2),]
   ))))
   for (g in 3:(n_group - 1)) {
     c_eta_X = cbind(c_eta_X, t(pnorm(X_weights %*% (
-      post_chain$post_eta[((g - 1) * dim_Xw + 1):(g * dim_Xw), ]
+      post_chain$post_eta[((g - 1) * dim_Xw + 1):(g * dim_Xw),]
     ))))
   }
   c_eta_X = cbind(c_eta_X, 1)
@@ -341,4 +342,3 @@ curve_ADJ <- function(x,
   #alternative : return(quantile(values, prob=0.5, na.rm=TRUE))
   return(values)
 }
-
